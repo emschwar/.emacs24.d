05e19be86ed1c9d0a8247f5d6a1f317bab216e46
Eric Schwartz <eschwartz@comverge.com>
2012-12-27 16:33:18 -0700

    Use gh-gist instead of gist, spell-check commit messages

--- init.d/github.el
+++ init.d/github.el
@@ -1,5 +1,9 @@
 ;; automatically do gists
-(require 'gist)
+(require 'gh-gist)
 (setq ems-github-netrc (netrc-machine ems-netrc "github" t))
 (setq gist-authenticate-function 'gist-basic-authentication)
 (setq gist-user-password (netrc-get ems-github-netrc "password"))
+
+(require 'git-commit)
+(add-hook 'git-commit-mode-hook 'turn-on-flyspell)
+(add-hook 'git-commit-mode-hook (lambda () (toggle-save-place 0)))
