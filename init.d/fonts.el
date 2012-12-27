b9d5f78b273b2aaae545a78620f3c44b9b4eba55
Eric Schwartz <eschwartz@comverge.com>
2012-12-27 16:31:38 -0700

    Using Source Code Pro now

--- init.d/fonts.el
+++ init.d/fonts.el
@@ -1,6 +1,6 @@
-(set-default-font "Menlo-16")
+(set-default-font "Source Code Pro-22")
 (defun fontify-frame (frame)
-  (set-frame-parameter frame 'font "Menlo-16"))
+  (set-frame-parameter frame 'font "Source Code Pro-22"))
 
 ;; Fontify current frame
 (fontify-frame nil)
