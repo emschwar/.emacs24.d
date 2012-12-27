1c1d991b066be1aadfe74cde303d7513e92d0f01
Eric Schwartz <eschwartz@comverge.com>
2012-12-27 16:31:16 -0700

    Add commands to add new lines above/below point

--- init.d/editing.el
+++ init.d/editing.el
@@ -6,3 +6,20 @@
 (setq frame-title-format
   '((buffer-file-name "%f" (dired-directory dired-directory "%b")) " - "
     invocation-name "@" system-name))
+
+(defun newline-previous ()
+  "Insert a blank line above the cursor and move the cursor up one line."
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (previous-line)
+  (indent-according-to-mode))
+(global-set-key (kbd "C-c C-p") 'newline-previous)
+
+;; From https://github.com/defunkt/textmate.el
+(defun newline-next ()
+  "Inserts an indented newline after the current line and moves the point to it."
+  (interactive)
+  (end-of-line)
+  (newline-and-indent))
+(global-set-key (kbd "C-c C-n") 'newline-next)
